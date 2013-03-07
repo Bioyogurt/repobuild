d23b32297c1ce01e44b8a6dae9047815e6db934c
Anton <weny@bk.ru>
2013-03-07 14:21:36 +0700

    Update gh_update.php
    End tests with autodeploy sources to server

--- www/gh_update.php
+++ www/gh_update.php
@@ -1,5 +1,4 @@
 <?php
-// test
 exec("cd /home/repobuild/git; git pull");
 exec("/usr/bin/rsync -crl /home/repobuild/git/www/ /home/repobuild/www/docs");
 exec("/usr/bin/rsync -crl /home/repobuild/git/scripts/ /home/repobuild/share/scripts");
