c0a821812ebf4147680ff5073b74924c022a297d
Anton <weny@bk.ru>
2013-03-07 14:52:09 +0700

    Update gh_update.php
    added delete parameter for rsync in gh_update

--- www/gh_update.php
+++ www/gh_update.php
@@ -1,4 +1,4 @@
 <?php
 exec("cd /home/repobuild/git; git pull");
-exec("/usr/bin/rsync -crl /home/repobuild/git/www/ /home/repobuild/www/docs");
-exec("/usr/bin/rsync -crl /home/repobuild/git/scripts/ /home/repobuild/share/scripts");
+exec("/usr/bin/rsync -crl --delete /home/repobuild/git/www/ /home/repobuild/www/docs");
+exec("/usr/bin/rsync -crl --delete /home/repobuild/git/scripts/ /home/repobuild/share/scripts");
