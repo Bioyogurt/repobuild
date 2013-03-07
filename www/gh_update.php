636a6a0ee159d0ae65e8cc06a24d3abac8561c24
Anton <weny@bk.ru>
2013-03-07 13:40:55 +0700

    Update gh_update.php
    test

--- www/gh_update.php
+++ www/gh_update.php
@@ -1,3 +1,5 @@
 <?php
-// yet another test2
+// tets
 exec("cd /home/repobuild/git; git pull");
+exec("rsync -curl /home/repobuild/git/www/ /home/repobuild/www/docs");
+exec("rsync -curl /home/repobuild/git/scripts/ /home/repobuild/share/scripts");
