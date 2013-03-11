2b5c274824844238cf86bb5a2113df2b59f65253
Anton <weny@bk.ru>
2013-03-11 15:00:20 +0700

    Update functions.php

--- www/inc/functions.php
+++ www/inc/functions.php
@@ -6,8 +6,7 @@ function dbc() {
     try {
         $params = array (
             PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES      => false
+            PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC
         );
         if($config['db']['pool'] && !$config['main']['debug'])
             $params[PDO::ATTR_PERSISTENT] = true;
