23d86a45cf1f4d26fdd6ab38ceae5bed45c17ae7
Anton <weny@bk.ru>
2013-03-11 16:53:54 +0700

    Update gh_update.php

--- www/gh_update.php
+++ www/gh_update.php
@@ -1,4 +1,2 @@
 <?php
 exec("cd /home/repobuild/share; git pull");
-#exec("/usr/bin/rsync -crl --delete /home/repobuild/git/www/ /home/repobuild/www/docs");
-#exec("/usr/bin/rsync -crl --delete /home/repobuild/git/scripts/ /home/repobuild/share/scripts");
