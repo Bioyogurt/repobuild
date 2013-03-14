d69a7e86e230463ab07069b54d99acf8eeccea0f
repobuild <repobuild@builder.(none)>
2013-03-14 11:16:29 +0700

    several fixes for build.php

--- scripts/build.php
+++ scripts/build.php
@@ -29,8 +29,8 @@ try {
 }
 
 /// Update packets versions
+$out = array();
 exec("rpm -qp --queryformat '%{NAME}:%{VERSION}\n' ".$config['main']['src_path']."*.rpm", $out);
-unset($out[0]);
 foreach($out as $o) {
     $o = explode(":", $o);
     $sth = $dbh->prepare("UPDATE packets SET version = :version WHERE name = :name");
@@ -38,40 +38,54 @@ foreach($out as $o) {
     $sth->bindParam(':name', $o[0]);
     try {
         $sth->execute();
-        $sth2 = $dbh->prepare("UPDATE builds SET builded = 'no' WHERE packet = (SELECT id FROM packets WHERE name = :name) AND version != :vertsion");
+        $sth2 = $dbh->prepare("UPDATE builds SET builded = 'no' WHERE packet = (SELECT id FROM packets WHERE name = :name LIMIT 1) AND version != :version");
         $sth2->bindParam(':version', $o[1]);
         $sth2->bindParam(':name', $o[0]);
-        $sth2->execute();
-    } catch(PDOException $e) {
+        try {
+            $sth2->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit(1);
+        }
+    } catch (PDOException $e) {
         echo $e->getMessage();
         exit(1);
     }
 }
 
 /// Build packets
-$sth = $dbh->prepare("SELECT DISTINCT (SELECT name FROM os WHERE id = repos.os) AS os, (SELECT name FROM archs WHERE id = repos.arch) AS arch, (SELECT name FROM packets WHERE id = builds.packet) AS name, (SELECT GROUP_CONCAT(IF(`value` IS NOT NULL, CONCAT((SELECT name FROM options WHERE id = `option`), '=', `value`), (SELECT name FROM options WHERE id = `option`)) separator ' ') FROM builds_opts WHERE build = builds.id) AS opts, `key`, (SELECT packets_list.`count` FROM packets_list WHERE packet_id = builds.packet) AS rpm_count FROM builds, repos WHERE repos.id = builds.repo AND `key` IS NOT NULL AND builded = 'no';");
+$sth = $dbh->prepare("SELECT DISTINCT ( SELECT `name` FROM os WHERE id = repos.os ) AS os, ( SELECT `name` FROM archs WHERE id = repos.arch ) AS arch, ( SELECT `name` FROM packets WHERE id = builds.packet ) AS `name`, ( SELECT GROUP_CONCAT( IF ( `value` IS NOT NULL, CONCAT(( SELECT `name` FROM `options` WHERE id = `option` ), '=', `value` ), ( SELECT `name` FROM `options` WHERE id = `option` )) SEPARATOR ' ' ) FROM builds_opts WHERE build = builds.id ) AS opts, `key`, ( SELECT packets_list.`count` FROM packets_list WHERE packet_id = builds.packet ) AS rpm_count, version AS ver1, ( SELECT packets.version FROM packets WHERE packets.id = builds.packet ) AS ver2 FROM builds, repos WHERE repos.id = builds.repo AND `key` IS NOT NULL AND builded = 'no';");
 $sth->execute();
 
 if($sth->rowCount() > 0) {
     $i = 0;
     while($row = $sth->fetch()) {
-        exec('ls -l '.$config['main']['builds_path'].$row['key'].'*.rpm | wc -l', $out);
-        print_r($out);
-        die();
-        $exec = 'mock -r '.$row['os'].'-'.$row['arch'].' --define="'.$row['name'].'_param '.$row['opts'].'" '.$config['main']['src_path'].$row['name'].'-*.src.rpm';
-        echo "\n\n\n".$i."\t".$exec."\n\n";
-        exec($exec, $out, $status);
-        if($status !== 0) {
-            die("ALARM! ALARM! ALARM!");
+        $build = true;
+        if(is_dir($config['main']['builds_path'].$row['key'])) {
+            $out = array();
+            exec('ls -l '.$config['main']['builds_path'].$row['key'].'/*.rpm | wc -l', $out);
+            if($out[0] == $row['rpm_count']) {
+                if($row['ver1'] == $row['ver2'])
+                    $build = false;
+            }
         }
 
-        $target_path = '/var/lib/mock/'.$row['os'].'-'.$row['arch'].'/result/';
-        exec('rm '.$target_path.'*debuginfo*');
-        exec('rm '.$target_path.'*.src.rpm');
-        if(!is_dir($config['main']['builds_path'].$row['key']))
-            mkdir($config['main']['builds_path'].$row['key']);
-        exec('mv '.$target_path.'*.rpm '.$config['main']['builds_path'].$row['key']);
-        $i++;
+        if($build) {
+            $exec = 'mock -r '.$row['os'].'-'.$row['arch'].' --define="'.$row['name'].'_param '.$row['opts'].'" '.$config['main']['src_path'].$row['name'].'-*.src.rpm';
+            echo "\n\n\n".$i."\t".$exec."\n\n";
+            exec($exec, $out, $status);
+            if($status !== 0) {
+                die("ALARM! ALARM! ALARM!");
+            }
+
+            $target_path = '/var/lib/mock/'.$row['os'].'-'.$row['arch'].'/result/';
+            exec('rm '.$target_path.'*debuginfo*');
+            exec('rm '.$target_path.'*.src.rpm');
+            if(!is_dir($config['main']['builds_path'].$row['key']))
+                mkdir($config['main']['builds_path'].$row['key']);
+            exec('mv '.$target_path.'*.rpm '.$config['main']['builds_path'].$row['key']);
+            $i++;
+        }
     }
 } else {
     echo "No packets for build\n";
@@ -92,8 +106,8 @@ if($sth->rowCount() > 0) {
 
 
         // Update packets versions
+        $out = array();
         exec("rpm -qp --queryformat '%{NAME}:%{VERSION}\n' ".$repopath."/*.rpm", $out);
-        unset($out[0]);
         foreach($out as $o) {
             $o = explode(":", $o);
             $sth2 = $dbh->prepare("UPDATE builds SET version = :version WHERE packet IN (SELECT id FROM packets WHERE name = :name)");
