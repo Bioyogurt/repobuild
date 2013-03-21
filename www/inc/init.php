64e8aacdd5ad4fff9a54e08095a8e2524e2ec197
Anton <weny@bk.ru>
2013-03-21 18:14:03 +0700

    Update init.php

--- www/inc/init.php
+++ www/inc/init.php
@@ -21,5 +21,11 @@ function __autoload($class_name) {
     include 'inc'.DS.'class.'.$class_name . '.php';
 }
 
+if(isset($_COOKIE['lang'])) {
+    $lang->set($_COOKIE['lang']);
+} else {
+    $lang->set('en');
+}
+
 if(!in_array($config['db']['engine'], PDO::getAvailableDrivers()))
     die('Not found <b>'.$config['db']['engine'].'</b> driver');
