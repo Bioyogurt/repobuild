1bb333b62d5c795b77236220c8581e7afbdccdae
Wendor <weny@bk.ru>
2013-03-21 10:59:12 +0700

    fix

--- www/inc/class.lang.php
+++ www/inc/class.lang.php
@@ -1,19 +1,19 @@
 <?php
 
 class lang {
-    private $strings = array();
-    private $lang;
+    private $_strings = array();
+    private $_lang;
     
     function __construct($lang='ru') {
-        $this->lang = $lang;
+        $this->_lang = $lang;
     }
     function __get($key) {
-        if(!isset($this->strings[$this->lang])) {
-            include 'inc'.DS.'langs'.DS.$this->lang.'.php';
+        if(!isset($this->_strings[$this->_lang])) {
+            include 'inc'.DS.'langs'.DS.$this->_lang.'.php';
             foreach($strings as $name => $string) {
-                $this->strings[$this->lang][$name] = $string;
+                $this->_strings[$this->_lang][$name] = $string;
             }
         }
-        return $this->strings[$this->lang][$key];
+        return $this->_strings[$this->_lang][$key];
     }
 }
